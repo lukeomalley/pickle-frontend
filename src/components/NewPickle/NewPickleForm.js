679914d2c33eb79e544a1bf2eebb8aceb7ff67d1
Luke O'Malley <Lukeomalley95@gmail.com>
2019-09-29 18:05:48 -0400

    added media queries to new pickle form to make responsive for mobile viewports

--- src/components/NewPickle/NewPickleForm.js
+++ src/components/NewPickle/NewPickleForm.js
@@ -4,7 +4,7 @@ import { useMutation } from '@apollo/react-hooks';
 import { withRouter } from 'react-router-dom';
 
 import Title from '../globals/Title';
-import { setRem, fadeIn, setTransition } from '../../styles';
+import { setRem, fadeIn, setTransition, sizes } from '../../styles';
 import CREATE_PICKLE from '../../mutations/CREATE_PICKLE';
 import ALL_PICKLE_QUERY from '../../queries/ALL_PICKLE_QUERY';
 
@@ -145,6 +145,25 @@ const NewPickleFormWrapper = styled.form`
     }
   }
 
+  select {
+    display: block;
+    font-size: inherit;
+    color: #444;
+    padding: ${setRem(12)} ${setRem(16)};
+    max-width: 100%;
+    margin: 0;
+    border: 1px solid #aaa;
+    border-radius: 0.5em;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    appearance: none;
+    background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
+      linear-gradient(to bottom, #ffffff 0%, #e5e5e5 100%);
+    background-repeat: no-repeat, repeat;
+    background-position: right 0.7em top 50%, 0 0;
+    background-size: 0.65em auto, 100%;
+  }
+
   h3 {
     text-align: center;
   }
@@ -155,5 +174,14 @@ const NewPickleFormWrapper = styled.form`
     input[type='radio'] {
     }
   }
+
+  @media (max-width: ${sizes.desktop}px) {
+    width: 80%;
+  }
+
+  @media (max-width: ${sizes.desktop}px) {
+    width: 90%;
+  }
 `;
+
 export default withRouter(NewPickleForm);
