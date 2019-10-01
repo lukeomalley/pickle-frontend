8897ab65dc664f688843db07a6434a2f643050a2
Luke O'Malley <Lukeomalley95@gmail.com>
2019-09-30 23:34:35 -0400

    fixed styles for longer text on pickle options

--- src/components/Pickles/PickleOptionVoted.js
+++ src/components/Pickles/PickleOptionVoted.js
@@ -5,8 +5,13 @@ import { setRem, fadeIn } from '../../styles';
 const OptionWrapper = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   border: 1px solid ${props => props.theme.lightGrey};
   padding: ${setRem(16)} ${setRem(32)};
+
+  .text {
+    margin-right: ${setRem(20)};
+  }
 
   ${fadeIn('0%', '0%', '0%', 0.8)}
 `;
@@ -15,7 +20,7 @@ const PickleOption = ({ option, selectedOptions }) => {
   const selected = option.id in selectedOptions;
   return (
     <OptionWrapper className={selected && 'green'}>
-      <div>{option.text}</div>
+      <div className="text">{option.text}</div>
       <div>{option.percentage}%</div>
     </OptionWrapper>
   );
